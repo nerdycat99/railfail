ae966ddf9fe90cf5b67b1d7de2a7667d0d99a4ae
nerdycat99 <info@drysky.co.uk>
2018-01-01 12:56:22 +0000

    fix for images missing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,12 @@ class StaticPagesController < ApplicationController
   def index
     @excuse = Excuse.order("RANDOM()").first
   end
+
+  def about
+  end
+
+  def fatcats
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  get 'fatcats', to: 'static_pages#fatcats'
 
   resources :excuses
 
